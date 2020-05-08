bf41418445d1a95ca0012cec1eefbe267bd3ed59
Dmitry <dimon.dudin1996@gmail.com>
2020-05-09 01:11:55 +0300

    Add view guestbook

--- Laravel/resources/views/guest.blade.php
+++ Laravel/resources/views/guest.blade.php
@@ -1,1 +1,15 @@
-Привет, мужчина
+
+<body style="align: center">
+<center>
+<h1>Гостевая книга</h1>
+<form >
+    <textarea cols="60" rows="5" placeholder="Введите текст" style="margin: 10px"></textarea><br>
+    <input type="text" placeholder="Автор"><br><br>
+    <input type="submit">
+</form>
+    <div style="width: 400px;height:200px;border: 1px solid gray;">
+    Контент
+    </div>
+</center>
+</body>
+
